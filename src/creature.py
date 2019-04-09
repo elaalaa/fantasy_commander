6debce63a22d22884516de617838c302b3ed5e29
Ella <ella.hakala@aalto.fi>
2019-04-09 16:45:58 +0300

    added movement rules

--- src/creature.py
+++ src/creature.py
@@ -1,4 +1,4 @@
-
+from location import Location
 
 class Creature():
     TANK = 0
@@ -14,20 +14,9 @@ class Creature():
         self.destroyed = False   # flag if character is destroyed
         self.set_type(type)      # creature type, defines movement and attack
         self.player = player
-        self.moving = False
-        self.attacking = False
-        self.set_hp()
+        #self.set_hp()
 
     
-    def set_hp(self):
-        if self.type == Creature.TANK:
-            self.hp = 30
-        elif self.type == Creature.MAGE:
-            self.hp = 10
-        elif self.type == Creature.NINJA:
-            self.hp = 20
-        elif self.type == Creature.SNIPER:
-            self.hp = 10
             
     def get_hp(self):
         return self.hp
@@ -94,11 +83,103 @@ class Creature():
             self.map = map
             self.location = location
             return True
-        
-    def attack(self, location):
-        pass
-
-
+    
 
     def __str__(self):
         return self.get_name() + ' at location ' + str(self.get_location())
+    
+    
+class Tank(Creature):
+       
+    def __init__(self, name, player, location):
+        super().__init__(name, Creature.TANK, player, location)
+        self.hp = 30
+    
+    def movement_squares(self):
+        squares = []
+        location = self.location
+        for x in range(location.x - 2, location.x + 3):
+            for y in range(location.y - 2, location.y + 3):
+                if self.map.contains(Location(x, y)) and self.map.get_tile(Location(x, y)).is_empty():
+                    squares.append(self.map.get_tile(Location(x, y)))
+        return squares
+    
+    def attack_squares(self):
+        pass
+    
+    def attack(self, location):
+        pass
+    
+    
+class Mage(Creature):
+    
+    def __init__(self, name, player, location):
+        super().__init__(name, Creature.MAGE, player, location)
+        self.hp = 10
+    
+    def movement_squares(self):
+        squares = []
+        location = self.location
+        for x in range(location.x - 1, location.x + 2):
+            for y in range(location.y - 1, location.y + 2):
+                if self.map.contains(Location(x, y)) and self.map.get_tile(Location(x, y)).is_empty():
+                    squares.append(self.map.get_tile(Location(x, y)))
+        return squares
+    
+    def attack_squares(self):
+        pass
+    
+    def attack(self, location):
+        pass
+    
+class Ninja(Creature):
+    
+    def __init__(self, name, player, location):
+        super().__init__(name, Creature.NINJA, player, location)
+        self.hp = 20
+        
+    def movement_squares(self):
+        squares = []
+        directions = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+        location = self.location
+        for direction in directions:
+            step = 1
+            while (1):
+                next = Location(location.x + (direction[0] * step), location.y + (direction[1] * step))
+                if self.map.contains(next):
+                    if self.map.get_tile(next).is_empty():
+                        squares.append(self.map.get_tile(next))
+                    else:
+                        break
+                else:
+                    break
+                step = step + 1
+        return squares
+    
+    def attack_squares(self):
+        pass
+    
+    def attack(self, location):
+        pass
+    
+    
+class Sniper(Creature):
+    
+    def __init__(self, name, player, location):
+        super().__init__(name, Creature.SNIPER, player, location)
+        self.hp = 10
+        
+    def movement_squares(self):
+        squares = []
+        location = self.location
+        for x in range(location.x - 1, location.x + 2):
+            for y in range(location.y - 1, location.y + 2):
+                if self.map.contains(Location(x, y)) and self.map.get_tile(Location(x, y)).is_empty():
+                    squares.append(self.map.get_tile(Location(x, y)))
+        return squares
+    
+    def attack_squares(self):
+        pass
+    
+    def attack(self, location):
+        pass
--- src/game.py
+++ src/game.py
@@ -38,14 +38,16 @@ class Game():
             self.print_msg("Select where to move")
             
     def move_action(self, location):
-        empty = self.map.get_tile(location).is_empty()
-        if empty == True:
-            self.map.get_tile(self.currentcreature.location).remove_creature()
-            self.currentcreature.location = location
-            self.map.get_tile(location).set_creature(self.currentcreature)
-            self.currentcreature = None
-            self.gamestate = Game.ATTACKSELECT
-            self.print_msg("Select creature to attack with")
+        squares = self.currentcreature.movement_squares()
+        if self.map.get_tile(location) in squares:
+            empty = self.map.get_tile(location).is_empty()
+            if empty == True:
+                self.map.get_tile(self.currentcreature.location).remove_creature()
+                self.currentcreature.location = location
+                self.map.get_tile(location).set_creature(self.currentcreature)
+                self.currentcreature = None
+                self.gamestate = Game.ATTACKSELECT
+                self.print_msg("Select creature to attack with")
             
     def attack_select(self, location):
         creature = self.map.get_creature(location)
@@ -87,82 +89,82 @@ def main():
     
 
     tank_location = Location(0, 0)
-    tank_body = Creature('Tank1', Creature.TANK, player1, tank_location)
+    tank_body = Tank('Tank1', player1, tank_location)
     test_map.add_creature(tank_body, tank_location)
     player1.add_teammember(tank_body)
 
     mage_location = Location(0, 1)
-    mage_body = Creature('Mage1', Creature.MAGE, player1, mage_location)
+    mage_body = Mage('Mage1', player1, mage_location)
     test_map.add_creature(mage_body, mage_location)
     player1.add_teammember(mage_body)
 
     ninja_location = Location(0, 2)
-    ninja_body = Creature('Ninja1', Creature.NINJA, player1, ninja_location)
+    ninja_body = Ninja('Ninja1', player1, ninja_location)
     test_map.add_creature(ninja_body, ninja_location)
     player1.add_teammember(ninja_body)
 
     sniper_location = Location(0, 3)
-    sniper_body = Creature('Sniper1', Creature.SNIPER, player1, sniper_location)
+    sniper_body = Sniper('Sniper1', player1, sniper_location)
     test_map.add_creature(sniper_body, sniper_location)
     player1.add_teammember(sniper_body)
     
     tank2_location = Location(0, 4)
-    tank2_body = Creature('Tank2', Creature.TANK, player1, tank2_location)
+    tank2_body = Tank('Tank2', player1, tank2_location)
     test_map.add_creature(tank2_body, tank2_location)
     player1.add_teammember(tank2_body)
 
     mage2_location = Location(0, 5)
-    mage2_body = Creature('Mage2', Creature.MAGE, player1, mage2_location)
+    mage2_body = Mage('Mage2', player1, mage2_location)
     test_map.add_creature(mage2_body, mage2_location)
     player1.add_teammember(mage2_body)
 
     ninja2_location = Location(0, 6)
-    ninja2_body = Creature('Ninja2', Creature.NINJA, player1, ninja2_location)
+    ninja2_body = Ninja('Ninja2', player1, ninja2_location)
     test_map.add_creature(ninja2_body, ninja2_location)
     player1.add_teammember(ninja2_body)
 
     sniper2_location = Location(0, 7)
-    sniper2_body = Creature('Sniper2', Creature.SNIPER, player1, sniper2_location)
+    sniper2_body = Sniper('Sniper2', player1, sniper2_location)
     test_map.add_creature(sniper2_body, sniper2_location)
     player1.add_teammember(sniper2_body)
     
     p2_tank_location = Location(13, 0)
-    p2_tank_body = Creature('Tank1', Creature.TANK, player2, p2_tank_location)
+    p2_tank_body = Tank('Tank1', player2, p2_tank_location)
     test_map.add_creature(p2_tank_body, p2_tank_location)
     player2.add_teammember(p2_tank_body)
 
     p2_mage_location = Location(13, 1)
-    p2_mage_body = Creature('Mage1', Creature.MAGE, player2, p2_mage_location)
+    p2_mage_body = Mage('Mage1', player2, p2_mage_location)
     test_map.add_creature(p2_mage_body, p2_mage_location)
     player2.add_teammember(p2_mage_body)
 
     p2_ninja_location = Location(13, 2)
-    p2_ninja_body = Creature('Ninja1', Creature.NINJA, player2, p2_ninja_location)
+    p2_ninja_body = Ninja('Ninja1', player2, p2_ninja_location)
     test_map.add_creature(p2_ninja_body, p2_ninja_location)
     player2.add_teammember(p2_ninja_body)
 
     p2_sniper_location = Location(13, 3)
-    p2_sniper_body = Creature('Sniper1', Creature.SNIPER, player2, p2_sniper_location)
+    p2_sniper_body = Sniper('Sniper1', player2, p2_sniper_location)
     test_map.add_creature(p2_sniper_body, p2_sniper_location)
     player2.add_teammember(p2_sniper_body)
     
     p2_tank2_location = Location(13, 4)
-    p2_tank2_body = Creature('Tank2', Creature.TANK, player2, p2_tank2_location)
+    p2_tank2_body = Tank('Tank2', player2, p2_tank2_location)
     test_map.add_creature(p2_tank2_body, p2_tank2_location)
     player2.add_teammember(p2_tank2_body)
 
     p2_mage2_location = Location(13, 5)
-    p2_mage2_body = Creature('Mage2', Creature.MAGE, player2, p2_mage2_location)
+    p2_mage2_body = Mage('Mage2', player2, p2_mage2_location)
     test_map.add_creature(p2_mage2_body, p2_mage2_location)
     player2.add_teammember(p2_mage2_body)
 
     p2_ninja2_location = Location(13, 6)
-    p2_ninja2_body = Creature('Ninja2', Creature.NINJA, player2, p2_ninja2_location)
+    p2_ninja2_body = Ninja('Ninja2', player2, p2_ninja2_location)
     test_map.add_creature(p2_ninja2_body, p2_ninja2_location)
     player2.add_teammember(p2_ninja2_body)
 
     p2_sniper2_location = Location(13, 7)
-    p2_sniper2_body = Creature('Sniper2', Creature.SNIPER, player2, p2_sniper2_location)
+    p2_sniper2_body = Sniper('Sniper2', player2, p2_sniper2_location)
     test_map.add_creature(p2_sniper2_body, p2_sniper2_location)
     player2.add_teammember(p2_sniper2_body)
     
